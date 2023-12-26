8282fac8ecadd9f656fa6050f496df3f8fcbd16d
Oxana <yurchenko.o@gmail.com>
2023-12-26 19:58:51 +0600

    added first styles

--- js/main.js
+++ js/main.js
@@ -61,18 +61,15 @@ function readTask() {
       contactCard.innerHTML = "";
       data.forEach((elem) => {
         console.log(elem);
-
+        list.innerHTML = "";
         contactCard.innerHTML += `
         <h5>${elem.nameOfCont}</h5>
         <h5>${elem.lName}</h5>
         <h5>${elem.phone}</h5>
-        <img class=".card_img" src="${elem.img}" alt="" />`;
-
-        list.innerHTML = "";
-
-        list.innerHTML += `
+        <img class=".card_img" src="${elem.img}" alt="" />
         <button id=${elem.id} class="btnDelete">delete</button>
-    <button id=${elem.id} class="btnEdit">edit</button>`;
+        <button id=${elem.id} class="btnEdit">edit</button>`;
+        contactCard.style.border = "1px solid black";
       });
     });
 }
@@ -80,17 +77,23 @@ readTask();
 
 // Удяляем карточку
 
+let deleteBtn = document.querySelector(".btnDelete");
+let editBtn = document.querySelector(".btnEdit");
 document.addEventListener("click", (e) => {
   let del_class = [...e.target.classList];
   console.log(del_class);
   if (del_class.includes("btnDelete")) {
     const del_id = e.target.id;
     //console.log(del_id);
+
     fetch(`${API}/${del_id}`, {
       method: "DELETE",
     }).then(() => readTask());
   }
 });
+
+// deleteBtn.style.display = "none";
+// editBtn.style.display = "none";
 
 //редактируем карточку
 let inpEdit = document.querySelector(".inpEdit");
@@ -141,101 +144,3 @@ function editedTask(editTask, id) {
     readTask();
   });
 }
-
-// let btn = document.querySelector(".btn");
-// let inp = document.querySelector(".task-input");
-// let list = document.querySelector(".task-list");
-
-// btn.addEventListener("click", () => {
-//   if (!inp.value.trim()) {
-//     alert("Заполните поле!");
-//     return;
-//   }
-//   let newObj = {
-//     task: inp.value,
-//   };
-//   createTask(newObj);
-//   readTask();
-//   inp.value = "";
-// });
-
-// function createTask(task) {
-//   fetch(API, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8",
-//     },
-//     body: JSON.stringify(task),
-//   }).then(() => readTask());
-// }
-
-// !=================READ================
-// function readTask() {
-//   fetch(API)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((data) => {
-//       list.innerHTML = "";
-//       data.forEach((elem) => {
-//         list.innerHTML += `<li>${elem.task}</li>
-//         <button id=${elem.id} class="btnDelete">delete</button>
-//     <button id=${elem.id} class="btnEdit">edit</button>`;
-//       });
-//     });
-// }
-// readTask();
-
-// !================Delete===================
-// document.addEventListener("click", (e) => {
-//   let del_class = [...e.target.classList];
-//   console.log(del_class);
-//   if (del_class.includes("btnDelete")) {
-//     const del_id = e.target.id;
-//     //console.log(del_id);
-//     fetch(`${API}/${del_id}`, {
-//       method: "DELETE",
-//     }).then(() => readTask());
-//   }
-// });
-
-// !========EDIT=====================
-// let inpEdit = document.querySelector(".inpEdit");
-// let btnEditSave = document.querySelector(".saveEdit");
-// let editModal = document.querySelector(".editModal");
-// document.addEventListener("click", (e) => {
-//   let edit_class = [...e.target.classList];
-//   if (edit_class.includes("btnEdit")) {
-//     editModal.style.display = "block";
-//     let id = e.target.id;
-//     fetch(`${API}/${id}`)
-//       .then((res) => {
-//         return res.json();
-//       })
-//       .then((data) => {
-//         inpEdit.value = data.task;
-//         btnEditSave.setAttribute("id", data.id);
-//       });
-//   }
-// });
-
-// btnEditSave.addEventListener("click", () => {
-//   let editTask = {
-//     task: inpEdit.value,
-//   };
-//   editedTask(editTask, btnEditSave.id);
-// });
-
-// function editedTask(editTask, id) {
-//   fetch(`${API}/${id}`, {
-//     method: "PATCH",
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8",
-//     },
-//     body: JSON.stringify(editTask),
-//   }).then(() => {
-//     readTask();
-//   });
-// }
-
-// kjasdfkljasdfkljasd
